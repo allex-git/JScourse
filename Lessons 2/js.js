544217d0f5562816044ad887ca8461dfd4eed697
allex-git <154755448+allex-git@users.noreply.github.com>
2023-12-30 19:45:09 +0200

    Update js.js

--- Lessons 2/js.js
+++ Lessons 2/js.js
@@ -7,6 +7,7 @@ if (!isNaN(num1 && num2)) {
   let b = num1 - num2
   let c = num1 * num2
   let d = num1 / num2
+  //Чи можна вираз alert та toHTML.innerHTML записати однією строкою?
   alert(num1 + ' + ' + num2 + ' = ' + a + '\n' + num1 + ' - ' + num2 + ' = ' + b + '\n' + num1 + ' * ' + num2 + ' = ' + c + '\n' + num1 + ' / ' + num2 + ' = ' + d)
   toHTML.innerHTML = (num1 + ' + ' + num2 + ' = ' + a + '<br>' + num1 + ' - ' + num2 + ' = ' + b + '<br>' + num1 + ' * ' + num2 + ' = ' + c + '<br>' + num1 + ' / ' + num2 + ' = ' + d)
 }
