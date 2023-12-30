3fa2153b35fc7d5cc95adca42b6c6fa121080466
allex-git <154755448+allex-git@users.noreply.github.com>
2023-12-30 23:30:44 +0200

    Update js.js

--- Lessons 2/js.js
+++ Lessons 2/js.js
@@ -1,17 +1,13 @@
 const num1 = Number(prompt('Enter number 1:'))
 const num2 = Number(prompt('Enter number 2:'))
 const toHTML = document.querySelector("#total")
-let result = `You enter not number`
 if (!isNaN(num1 && num2)) { 
   let a = num1 + num2
   let b = num1 - num2
   let c = num1 * num2
   let d = num1 / num2
-  //Чи можна вираз alert та toHTML.innerHTML записати однією строкою?
   alert(num1 + ' + ' + num2 + ' = ' + a + '\n' + num1 + ' - ' + num2 + ' = ' + b + '\n' + num1 + ' * ' + num2 + ' = ' + c + '\n' + num1 + ' / ' + num2 + ' = ' + d)
-  toHTML.innerHTML = (num1 + ' + ' + num2 + ' = ' + a + '<br>' + num1 + ' - ' + num2 + ' = ' + b + '<br>' + num1 + ' * ' + num2 + ' = ' + c + '<br>' + num1 + ' / ' + num2 + ' = ' + d)
 }
 else {
-  alert(result)
-  toHTML.innerHTML = result
+  alert(`You enter not number`)
 } 
