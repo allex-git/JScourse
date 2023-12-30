fef9bcf62256ee31def6bad11adddb1cea34a852
allex-git <154755448+allex-git@users.noreply.github.com>
2023-12-30 19:27:35 +0200

    Update js.js

--- Lessons 2/js.js
+++ Lessons 2/js.js
@@ -1,26 +1,16 @@
 const num1 = Number(prompt('Enter number 1:'))
 const num2 = Number(prompt('Enter number 2:'))
-
-
+const toHTML = document.querySelector("#total")
+let result = `You enter not number`
 if (!isNaN(num1 && num2)) { 
   let a = num1 + num2
   let b = num1 - num2
   let c = num1 * num2
   let d = num1 / num2
   alert(num1 + ' + ' + num2 + ' = ' + a + '\n' + num1 + ' - ' + num2 + ' = ' + b + '\n' + num1 + ' * ' + num2 + ' = ' + c + '\n' + num1 + ' / ' + num2 + ' = ' + d)
-
-  const toHTML = document.querySelector(`.total`)
-    if (Number) {
-    const math1 = `${num1} + ${num2} = ${a} \n ${num1} - ${num2} = ${b} \n ${num1} * ${num2} = ${c} \n ${num1} / ${num2} = ${d} \n `
-    toHTML.innerText = math1
-    console.log( math1)
-    } 
-    else if (isNaN) {
-      
-      const emptyText = `You enter not number`
-  toHTML.innerText = emptyText
-  console.log(emptyText)
- 
-  alert(emptyText)
- } 
-  }
+  toHTML.innerHTML = (num1 + ' + ' + num2 + ' = ' + a + '<br>' + num1 + ' - ' + num2 + ' = ' + b + '<br>' + num1 + ' * ' + num2 + ' = ' + c + '<br>' + num1 + ' / ' + num2 + ' = ' + d)
+}
+else {
+  alert(result)
+  toHTML.innerHTML = result
+} 
