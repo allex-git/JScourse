c6ade664b1eb3747575bff54c7c6fb1e91cefea8
allex-git <154755448+allex-git@users.noreply.github.com>
2024-01-09 21:45:58 +0200

    Add files via upload

---
+++ Lessons3.Tesk4/script.js
@@ -0,0 +1,7 @@
+
+let numFirst = Number(prompt(`Введіть перше число:`))
+let numSecond = Number(prompt(`Введіть друге число:`))
+let numThird = Number(prompt(`Введіть третє число:`))
+let result = (numFirst + numSecond + numThird) / 3
+alert(`середнє арифметичне цих чисел: ${result}`)
+
