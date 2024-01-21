c3d0d5c53f87e1a81ffcd94f3634906f9df204fd
allex-git <154755448+allex-git@users.noreply.github.com>
2024-01-21 13:41:48 +0200

    Add files via upload

---
+++ Lessons6.Task10/script.js
@@ -0,0 +1,22 @@
+let a = Number(prompt(`Введите переменную "а":`))
+let b = Number(prompt(`Введите переменную "b":`))
+function main(a, b ,c) {
+    if (!a){
+        a = 2
+    }
+    if (!b){
+        b = 3
+    }
+    if (typeof c === `function`){
+        return c(sum(a,b)) 
+    } else {
+        return sum (a,b)
+    }
+}
+function sum(a, b) {
+    return a + b
+}
+let result = main(a,b, (num) => {
+    return num - 1
+})
+console.log(result)
