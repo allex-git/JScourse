9ef5ffacfda0ea2f24f611497480df3b5ed44f5d
allex-git <154755448+allex-git@users.noreply.github.com>
2024-01-24 16:49:42 +0200

    Add files via upload

---
+++ Lessons7.Task12/script.js
@@ -0,0 +1,19 @@
+let hour = Number(prompt('Введіть кількість годин:'))
+const secondsInHours = () => hour *3600
+if (isNaN(hour)) {
+    alert(`Ви ввели не число`)
+}
+else if (hour === 0) {
+    alert(`Серйозно?`)
+    }
+else if (hour === 1) {
+    alert(`В ${hour} годині ${secondsInHours()} секунд`)
+    }
+else if (hour === null) {
+    alert(`Ви відмовилися від введення!`)
+    }
+else if (hour == 0) {
+    alert(`Ви нічого не ввели!`)
+    } 
+else { alert(`В ${hour} годинах ${secondsInHours()} секунд`)
+}
