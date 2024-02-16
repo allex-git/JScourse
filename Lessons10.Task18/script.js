538bbdcf0e5bc39f545293735fbe7b5e31e6940a
allex-git <154755448+allex-git@users.noreply.github.com>
2024-02-16 19:44:49 +0200

    script.js

--- Lessons10.Task18/script.js
+++ Lessons10.Task18/script.js
@@ -1,114 +1,53 @@
-function getRandRange(min, step) {
-    return Math.ceil(Math.random() * step) + min
-  }
-  
-  function setWinner(isLeft) {
-    alert(`${isLeft ? heroRight.name : heroLeft.name} is Winner`)
-    heroRight.startGame()
-    heroLeft.startGame()
-  }
-  
-  const attackerParts = [
-    'головою',
-    'рукою',
-    'ногою',
-    'пальцем',
-    'носом',
-  ]
-  
-  const attackieParts = [
-    'голову',
-    'руку',
-    'ногу',
-    'палець',
-    'ніс',
-  ]
-  
-  class Hero {
-    constructor(name, hp, className, img, str) {
-      this.name = name
-      this.hp = hp
-      this.maxHp = hp
-      this.className = className
-      this.img = img
-      this.str = str
+class Human {
+    constructor(names, age) {
+      this.names = names
+      this.age = age 
     }
-  
-    startGame() {
-      this.hp = this.maxHp
-      const hpText = document.querySelector(`.${this.className} .hp-text`)
-      const hpHeight = document.querySelector(`.${this.className} .hp`)
-      hpText.innerHTML = this.maxHp
-      hpHeight.style.height = `100%`
+    infoHuman() {
+      return console.log(`Human name: ${this.names}, Human age: ${this.age}`)
     }
-  
-    setHp(isStart) {
-      const isBlock = Math.random() < 0.3
-      const value = (isStart || isBlock) ? 0 : this.str
-      const hpText = document.querySelector(`.${this.className} .hp-text`)
-      const hpHeight = document.querySelector(`.${this.className} .hp`)
-  
-      const newHpValue = this.hp - value
+}
+
+class Automobile {
+  owner;
+    constructor(vendor, model, year, registerNumber) {
+      this.vendor = vendor
+      this.model = model
+      this.year = year
+      this.registerNumber = registerNumber
+    }
+
+    set owner(human) {
+        this.owner = human;
+    };
+
+    carInfo() {
       
-      const newValue = (newHpValue < 0) ? 0 : newHpValue
-      
-      hpText.innerHTML = newValue
-      hpHeight.style.height = `${newValue * 100 / this.maxHp || 2}%`
-      this.hp -= value
-  
-      if(!isBlock && !isStart) {
-        const logBlock = document.querySelector('.battle-log')
-        const logPostBlock = document.createElement('div')
-        const logPost = document.createElement('span')
-        const logPostImg = document.createElement('img')
-  
-        logPostImg.src = this.img
-  
-        logPost.innerText = `${this.name} наніс удар ${attackerParts[Math.floor(Math.random() * attackerParts.length)]} в ${attackieParts[Math.floor(Math.random() * attackieParts.length)]}, ${this.className === 'left' ? heroRight.name : heroLeft.name} плаче !`
-  
-        logPostBlock.appendChild(logPostImg)
-        logPostBlock.appendChild(logPost)
-        logBlock.appendChild(logPostBlock)
-      }
-  
-      if (newHpValue < 1) {
-        this.hp = this.maxHp
-        setWinner(this.className === 'left')
-      }
-      // setTimeout(() => {
-      //   if (isBlock) console.log(`${this.name} have blocked the damage !!!`)
-      // }, 100)
+      console.log(this.owner.age > 18 ? `Автомобіль марки ${this.vendor} ${this.model}, ${this.year} року випуску з реєстраційним номером ${this.registerNumber}` : `Нажаль вік майбутнього власника меньшe 18, а саме ${this.owner.age}`)
+      this.owner.infoHuman();
+      console.log(`-----------------------------------------------------------------------------------------------------------------------------------------------`)
     }
-  
-    setName() {
-      const nameEl = document.createElement('h3')
-      nameEl.classList.add('hero-name')
-      nameEl.innerHTML = this.name
-      document.querySelector(`.${this.className}`).appendChild(nameEl)
-    }
-  
-    setImage() {
-      const imageEl = document.querySelector(`.${this.className} img`)
-      imageEl.setAttribute('src', this.img)
-    }
-  }
-  
-  const heroLeft = new Hero('Mordekaiser', getRandRange(100, 300), 'left', 'https://static.wikia.nocookie.net/leagueoflegends/images/f/f1/Mordekaiser_Render.png/revision/latest/thumbnail/width/360/height/360?cb=20190604005742', getRandRange(12, 28))
-  
-  const heroRight = new Hero('Katarina', getRandRange(100, 100), 'right', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/16ba7581-d40f-45a6-ad94-b1e783d22fb6/de0lpnj-0c5cfa07-ff66-4790-9e9b-8c580740b972.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE2YmE3NTgxLWQ0MGYtNDVhNi1hZDk0LWIxZTc4M2QyMmZiNlwvZGUwbHBuai0wYzVjZmEwNy1mZjY2LTQ3OTAtOWU5Yi04YzU4MDc0MGI5NzIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.-BUbFjxIB3dR59THK7ZdC_NUuBSmt6Mr59ek2zxL7Wk', getRandRange(12, 8))
-  
-  const hitBtn = document.querySelector('.hit-btn')
-  
-  hitBtn.addEventListener('click',() => {
-    heroLeft.setHp()
-    heroRight.setHp()
-  })
-  
-  
-  
-  heroLeft.setHp(true)
-  heroRight.setHp(true)
-  heroRight.setName()
-  heroLeft.setName()
-  heroLeft.setImage()
-  heroRight.setImage()
+}
+
+const personHumanA = new Human(`Anna`, 19)
+const personHumanB = new Human(`Bogdan`, 35)
+const personHumanC = new Human(`Yurii`, 15)
+const personHumanD = new Human(`Olga`, 31)
+const personHumanE = new Human(`Pavlo`, 17)
+
+const mazda = new Automobile(`Mazda`, `CR-7`, 2019, `AA2323BH`, `Vadym`, 22)
+mazda.owner = personHumanA
+const vw = new Automobile(`VW`, `Passat`, 2015, `AX4503HI`, `Yurii`, 15)
+vw.owner = personHumanB
+const ford = new Automobile(`Ford`, `Focus`, 2021, `BC3712CB`)
+ford.owner = personHumanC
+const opel = new Automobile(`Opel`, `Omega`, 2009, `BI4511AE`)
+opel.owner = personHumanD
+const audi = new Automobile(`Audi`, `Q5`, 2015, `AT2693BC`)
+audi.owner = personHumanE
+
+mazda.carInfo();
+vw.carInfo();
+ford.carInfo();
+opel.carInfo();
+audi.carInfo();
